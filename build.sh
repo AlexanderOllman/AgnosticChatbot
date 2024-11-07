1b51d1e671528cf210c4a98fce1ee6ad57949558
AlexanderOllman <alexander.ollman@gmail.com>
2024-11-06 20:44:15 -0800

    ya wally

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo "Building image..."
 docker build -t fheonix/agnostic-chatbot:0.0.1 .
 
 echo "Pushing image..."
-docker push fheonix/agnostic-chatbotr:0.0.1
+docker push fheonix/agnostic-chatbot:0.0.1
 
 echo "Done."
 
